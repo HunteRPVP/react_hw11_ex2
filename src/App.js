681888a69b63bbf2d1f1263154a96e96d305fa23
HunteRPVP <55155885+HunteRPVP@users.noreply.github.com>
2021-06-27 21:41:17 +0300

    done

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import React, { useState, useEffect } from "react";
+import TodoList from "./TodoList";
+
+export default function App() {
+  const [todos, setTodos] = useState([]);
+
+  const [todoTitle, setTodoTitle] = useState("");
+
+  const handleClick = () => console.log("click");
+
+  useEffect(() => {
+    const raw = localStorage.getItem("todos") || [];
+    setTodos(JSON.parse(raw));
+  }, []);
+
+  useEffect(() => {
+    document.addEventListener("click", handleClick);
+    localStorage.setItem("todos", JSON.stringify(todos));
+    return () => {
+      document.removeEventListener("click", handleClick);
+    };
+  }, [todos]);
+
+  const addTodo = (event) => {
+    if (event.key === "Enter") {
+      setTodos([
+        ...todos,
+        {
+          id: Date.now(),
+          title: todoTitle,
+          completed: false,
+        },
+      ]);
+      setTodoTitle("");
+    }
+  };
+
+  return (
+    <div className="container">
+      <h1>Todo app</h1>
+
+      <div className="input-field">
+        <input
+          type="text"
+          value={todoTitle}
+          onChange={(event) => setTodoTitle(event.target.value)}
+          onKeyPress={addTodo}
+        />
+        <label className="active">Todo name</label>
+      </div>
+
+      <TodoList todos={todos} />
+    </div>
+  );
+}
